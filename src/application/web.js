d765ad91e27cd7471415272805a11a50642fce40
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2024-06-18 20:54:23 +0700

    debug product

--- src/application/web.js
+++ src/application/web.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../route/public-api.js";
 import { authRouter } from "../route/auth-api.js";
@@ -7,18 +6,12 @@ import path from "path";
 
 export const web = express();
 
+web.use();
+
 // Serve static files from the 'public' directory
 const __dirname = path.resolve();
 web.use(express.static(path.join(__dirname, "public")));
 
-const corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-
-web.use(cors(corsOptions));
 web.use(express.json());
 web.use(publicRouter);
 web.use(authRouter);
