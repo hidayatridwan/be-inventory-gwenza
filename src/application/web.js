651f8e4860adea70d60a01966ae7a77722146177
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2024-06-18 21:20:46 +0700

    debug product

--- src/application/web.js
+++ src/application/web.js
@@ -7,7 +7,28 @@ import cors from "cors";
 
 export const web = express();
 
-web.use(cors());
+// Define your allowed origins
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "http://86.38.204.197:3001",
+  // Add other allowed origins here
+];
+
+// CORS middleware configuration
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // If you need to support cookies
+  })
+);
 
 // Serve static files from the 'public' directory
 const __dirname = path.resolve();
