cc27ac1cfa6c91cddb4658e9042078a3273cf2d5
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2024-06-18 21:09:37 +0700

    debug product

--- src/application/web.js
+++ src/application/web.js
@@ -3,10 +3,11 @@ import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../route/public-api.js";
 import { authRouter } from "../route/auth-api.js";
 import path from "path";
+import cors from "cors";
 
 export const web = express();
 
-web.use();
+web.use(cors());
 
 // Serve static files from the 'public' directory
 const __dirname = path.resolve();
