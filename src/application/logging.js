799bd52f693aecf3f80277432f440b997b50ade8
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2024-06-22 14:18:16 +0700

    adding dailiy rotate log file

--- src/application/logging.js
+++ src/application/logging.js
@@ -1,9 +1,16 @@
 import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
 
 export const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: "logs/application.log" }),
+    new DailyRotateFile({
+      filename: "logs/application-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "14d",
+    }),
   ],
 });
--- src/error/response-error.js
+++ src/error/response-error.js
@@ -1,7 +1,11 @@
+import { logger } from "../application/logging.js";
+
 class ResponseError extends Error {
   constructor(status, message) {
     super(message);
     this.status = status;
+
+    logger.error(`status: ${status}, message: ${message}`);
   }
 }
 
